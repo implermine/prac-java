5dac115f93c083657c9cfede805438ad08bc24e0
이태강 <lkorea12345@encar.com>
2022-07-25 10:38:57 +0900

    feat: .

---
+++ src/test/java/apache/commons/collection4/EntryPoint.java
@@ -0,0 +1,9 @@
+package apache.commons.collection4;
+
+public class EntryPoint {
+
+    public static void main(String[] args) {
+
+
+    }
+}
--- src/test/java/polymorphism/casting.java
+++ src/test/java/polymorphism/casting.java
@@ -1,27 +1,56 @@
 package polymorphism;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
 public class casting {
 
-    @AllArgsConstructor
-    @NoArgsConstructor
     public static class Base{
         public String name;
         public String a;
+
+        public Base(String name, String a) {
+            this.name = name;
+            this.a = a;
+        }
+
+        public Base() {
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setA(String a) {
+            this.a = a;
+        }
     }
 
-    @AllArgsConstructor
     public static class Derived extends Base{
         public String name;
         public String b;
+
+        public Derived(String name, String b) {
+            this.name = name;
+            this.b = b;
+        }
+
+        public Derived() {
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setB(String b) {
+            this.b = b;
+        }
     }
 
     public static void main(String[] args) {
-        Base base = new Base("Base", "1");
-        Derived downCastedBase = (Derived) base;
-        // 원상복귀가 아닌 다운캐스팅은 불가함.
+//        Base base = new Base("Base", "1");
+//        Derived downCastedBase = (Derived) base;
+//        // 원상복귀가 아닌 다운캐스팅은 불가함.
+
+        Derived derived = new Derived();
+        derived.setA("A"); // -> we can set A that is field of Base Class
     }
 
 
